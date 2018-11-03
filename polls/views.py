17bb8fd032f44e315ac3392c4122c7aadec1e0a9
robert neuzil <robertneuzil@gmail.com>
2018-11-03 00:57:39 -0400

    included urls

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def polls_list(request):
+	return HttpResponse("<h1> polls_list page </h1>")
+
 
 # Create your views here.
