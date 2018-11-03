f3b1c200820061e4edc861da50b3f4ab05ab9790
robert neuzil <robertneuzil@gmail.com>
2018-11-02 22:12:05 -0400

    models created and registered

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Poll(models.Model):
+	
+	text = models.CharField(max_length=255)
+	pub_date = models.DateField()
+
+	def __str__(self):
+		return f'{self.text[:20]}'
 # Create your models here.
