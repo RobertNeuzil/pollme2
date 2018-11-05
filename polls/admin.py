68cfd64393451929fefaeb7aee42aeea3c296bb9
robert neuzil <robertneuzil@gmail.com>
2018-11-04 22:51:28 -0500

    foreign key models

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Poll
+from .models import Poll, Choice
 
 
 admin.site.register(Poll)
+admin.site.register(Choice)
 
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -8,4 +8,11 @@ class Poll(models.Model):
 
 	def __str__(self):
 		return f'{self.text}'
-# Create your models here.
+
+class Choice(models.Model):
+	question = models.ForeignKey(Poll, on_delete = models.CASCADE)
+	choice_text = models.CharField(max_length=255)
+	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return f'{self.question.text} - {self.choice_text}'
